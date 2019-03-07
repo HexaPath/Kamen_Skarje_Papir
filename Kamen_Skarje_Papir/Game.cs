a7255f07d2b3ebed9c256730d79df6be57e46e4d
HexaPath <43265617+HexaPath@users.noreply.github.com>
2019-03-07 08:39:00 +0100

    Beautify

--- Kamen_Skarje_Papir/Game.cs
+++ Kamen_Skarje_Papir/Game.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Kamen_Skarje_Papir
+﻿namespace Kamen_Skarje_Papir
 {
     public class Game
     {
